1a6977e16968dee7e972ba2f95d3b241afd55cda
jason-choi-skku <jasonchoi3@g.skku.edu>
2022-05-19 01:54:47 +0900

    [ADD] corrlation.csv

--- src/components/Main.ts
+++ src/components/Main.ts
@@ -1,13 +1,34 @@
 import { data } from "../types";
 import * as d3 from "d3";
 
-class Main {
+export class Main {
     matchData: data;
+    correlationData : data;
 
     constructor() {
-        d3.csv("data/matchData.csv").then( (d : any) => {
+        d3.csv(
+            "https://raw.githubusercontent.com/Jason-Choi/lol/master/data.csv"
+        ).then((d: any) => {
             this.matchData = d;
-            console.log(this.matchData);
+            
         });
+
+        
     }
 }
+
+const template = /* html */ `
+    <div class="container" id="main">
+        <div class="row">
+            <div class="col-2">
+                BLUE TEAM
+            </div>
+            <div class="col-8">
+                Main
+            </div>
+            <div class="col-2">
+                RED TEAM
+            </div>
+        </div>
+    </div>
+`;
